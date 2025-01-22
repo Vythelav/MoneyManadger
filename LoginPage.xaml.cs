1856b65b6176443c35fe57e3aa4c0bfea9c2064b
admin <admin@510EC15>
2025-01-22 12:36:48 +0300

    rdf

--- LoginPage.xaml.cs
+++ LoginPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,16 +21,60 @@ namespace MoneyManadger
     /// </summary>
     public partial class LoginPage : Page
     {
+        private string connectionString = "Server=510EC15;Database=MoneyManager;Trusted_Connection=True;";
         public LoginPage()
         {
             InitializeComponent();
         }
         private void Login_Button_Click(object sender, RoutedEventArgs e)
         {
-            ManagerWindow managerWindow = new ManagerWindow();
-            managerWindow.Show();
-
+            string username = LoginTextBox.Text;
+            string password = PasswordTextBox.Password;
             
+            User user = ValidateUser(username, password);
+            if (user != null)
+            {
+                ManagerWindow managerWindow = new ManagerWindow();
+            managerWindow.Show();
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+        private User ValidateUser(string username, string password)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT Name FROM Users WHERE Name = @username AND Password = @password";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@username", username);
+                        command.Parameters.AddWithValue("@password", password);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                return new User
+                                {
+                                    
+                                    Name = reader["Name"].ToString()
+                                };
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            return null;
         }
 
         private void RegistrateTextClick(object sender, MouseButtonEventArgs e)
