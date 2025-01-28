c1111724934c03dcf98dab543e81eb3862fb52b6
админ <slava.gusaov@gmail.com>
2025-01-28 21:31:39 +0300

    cr

--- View/SettingPage.xaml.cs
+++ View/SettingPage.xaml.cs
@@ -1,28 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MoneyManadger
 {
-    /// <summary>
-    /// Логика взаимодействия для SettingPage.xaml
-    /// </summary>
     public partial class SettingPage : Page
     {
         public SettingPage()
         {
             InitializeComponent();
+            App.ApplyTheme(this);
+        }
+
+        private void ApplyTheme()
+        {
+            ToggleThemeButton.Content = App.IsDarkTheme ? "Включить светлую тему" : "Включить темную тему";
+
+            var stackPanel = this.FindName("MainStackPanel") as StackPanel;
+            foreach (var child in stackPanel.Children)
+            {
+                if (child is Button button)
+                {
+                    button.Background = App.IsDarkTheme
+                        ? (SolidColorBrush)Application.Current.Resources["SecondColor"]
+                        : (SolidColorBrush)Application.Current.Resources["ElementsColor"];
+                }
+            }
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ToggleThemeButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            App.IsDarkTheme = !App.IsDarkTheme;
+
+            foreach (Window window in Application.Current.Windows)
+            {
+                App.ApplyTheme(window);
+            }
+
+            ApplyTheme(); 
         }
     }
 }
